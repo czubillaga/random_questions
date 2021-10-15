4abaeb55518c07ff8019aec72155453594675a5b
Carlos Zubillaga <carloszubillaga@Carloss-MacBook-Pro.local>
2021-10-15 16:01:15 +0100

    fixed xml symbols

--- src/components/QuestionBox.js
+++ src/components/QuestionBox.js
@@ -1,10 +1,12 @@
 import React from "react";
+import unescape from 'lodash/unescape';
 
 const QuestionBox = ({currentQuestion}) => {
 
+
     return(
         <div class="question-box">
-            {currentQuestion ? <h4>{currentQuestion.question}</h4>: null}
+            {currentQuestion ? <h4>{unescape(currentQuestion.question)}</h4> : null}
         </div>
     )
 }
